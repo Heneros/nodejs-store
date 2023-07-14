3c7508ae04f825080a7803d25b8faaeeb5cfc1d2
Heneros <punk60826@gmail.com>
2023-07-15 01:02:21 +0300

    products.js

--- controllers/products.js
+++ controllers/products.js
@@ -4,12 +4,13 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
     ///.sort('-name price')
-    const products = await Product.find({}).select('name price')
+    const products = await Product.find({}).sort('name').select('name price').limit(10).skip(5)
     res.status(200).json({ products, countProducts: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort } = req.query;///деструктризация объекта req.query
+    ///fields поиск по конкретным значением в search
+    const { featured, company, name, sort, fields } = req.query;///деструктризация объекта req.query
     const queryObject = {}; ///Создает фильтр для  фильтраци запроса к базе данных.
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false; ///преобразованиt строки 'true' в соответствующее булевое значение, чтобы оно соответствовало требуемому формату для фильтрации запроса.
@@ -29,6 +30,13 @@ const getAllProducts = async (req, res) => {
     } else {
         result = result.sort('createdAt');
     }
+    if (fields) {
+        const fieldsList = sort.split(',').join(' ');
+        result = result.select(fieldsList);
+    }
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+
     const products = await result;
     console.log(req.query); ///Вернет отсюда {{URL}}/products?featured=true  Все продукты с true значением {  featured: 'true' }
     res.status(200).json({ products, countProducts: products.length })
